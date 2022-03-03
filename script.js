2cdd364240b5742795c334aa7945bfa926b938c6
Felipe <fe.peressim@gmail.com>
2022-03-03 17:52:32 -0300

    Task 3: Added a button that shuffles the deck and shows the cards in random order.

--- script.js
+++ script.js
@@ -3,16 +3,25 @@ const displayCards = document.querySelector(".display-cards");
 const naipes = ["Paus", "Copas", "Ouros", "Espadas"];
 const cards = [];
 
-function HandleEventListener(event) {
-  const naipeToFilter = event.currentTarget.textContent;
-
-  CreateCardsElements(naipeToFilter);
+function handleEventListener(event) {
+  const elementTextContent = event.currentTarget.textContent;
+  
+  switch (elementTextContent) {
+  case 'Reset':
+    createCardsElements(cards);
+    break;
+  case 'Embaralhar':
+    createCardsElements(shuffleArray(cards));
+    break;
+  default:
+    createCardsElements(filterNaipe(elementTextContent));
+    break;
+  }
 }
 
-function CreateCardsElements(naipeToFilter) {
-  const filtered = naipeToFilter == 'Reset' ?  cards : FilterNaipe(naipeToFilter);
-    
-  nodeList = filtered.map((n) => {
+function createCardsElements(cards) {
+  console.log(cards);
+  nodeList = cards.map((n) => {
     const newDiv = document.createElement("div");
     const newNaipe = document.createElement("p");
     newNaipe.textContent = `${n.numero} - ${n.naipe}`;
@@ -20,44 +29,51 @@ function CreateCardsElements(naipeToFilter) {
     newDiv.classList.add("card");
     return newDiv;
   });
-
+  
   return displayCards.replaceChildren(...nodeList);
 }
 
-function FilterNaipe(naipeToFilter) {
+function filterNaipe(naipeToFilter) {
   return cards.filter(card =>  card.naipe === naipeToFilter);
 }
 
 function createCardsArray() {
-    for (let a = 0; a < naipes.length; a++) {
-	for (let i = 0; i < 13; i++) {
-	    switch (i) {
-	    case 0:
-		obj = { naipe: naipes[a], numero: "A" };
-		break;
-	    case 10:
-		obj = { naipe: naipes[a], numero: "J" };
-		break;
-	    case 11:
-		obj = { naipe: naipes[a], numero: "Q" };
-		break;
-	    case 12:
-		obj = { naipe: naipes[a], numero: "K" };
-		break;
-	    default:
-		obj = { naipe: naipes[a], numero: `${i + 1}` };
-	    }
-	    cards.push(obj);
-	}
+  for (let a = 0; a < naipes.length; a++) {
+    for (let i = 0; i < 13; i++) {
+      switch (i) {
+      case 0:
+	obj = { naipe: naipes[a], numero: "A" };
+	break;
+      case 10:
+	obj = { naipe: naipes[a], numero: "J" };
+	break;
+      case 11:
+	obj = { naipe: naipes[a], numero: "Q" };
+	break;
+      case 12:
+	obj = { naipe: naipes[a], numero: "K" };
+	break;
+      default:
+	obj = { naipe: naipes[a], numero: `${i + 1}` };
+      }
+      cards.push(obj);
     }
+  }
 }
 
 function addEventListenerToButtons() {
-    buttons.forEach((b) => {
-  b.addEventListener("click", (event) => HandleEventListener(event));
-});
+  buttons.forEach((b) => {
+    b.addEventListener("click", (event) => handleEventListener(event));
+  });
 } 
 
-createCardsArray()
-addEventListenerToButtons()
-CreateCardsElements('Reset');
+function shuffleArray(array) {
+  return  array
+    .map(card => ({ card, value: Math.random() }))
+    .sort((a, b) => a.value - b.value)
+    .map(obj => obj.card);
+}
+
+createCardsArray();
+addEventListenerToButtons();
+createCardsElements(cards);
